1ce913745bdc0b837a514e3237e9e0c4da03c24b
Nathan Weber <N-weber@live.fr>
2024-11-06 16:32:22 +0100

    add search bar

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,47 @@ class MyappBar extends StatelessWidget implements PreferredSizeWidget {
 class AddSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(height: 200, color: Colors.blue);
+    return Container(
+      color: Colors.grey[200],
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 4,
+                          offset: Offset(0, 3))
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Input',
+                        contentPadding: EdgeInsets.all(10),
+                        border: InputBorder.none),
+                  ),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 100,
+                color: Colors.green,
+              )
+            ],
+          ),
+          Container(
+            height: 50,
+            color: Colors.red,
+          )
+        ],
+      ),
+    );
   }
 }
